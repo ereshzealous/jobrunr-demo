edcdf82256d81f7cc9a066381bbb669159a1d24c
Eresh Gorantla <eresh.gorantla@getir.com>
2022-04-12 08:27:02 +0530

    Upgraded Jobrunr version to 5.0.0

--- src/main/java/com/spring/jobrunr/api/JobController.java
+++ src/main/java/com/spring/jobrunr/api/JobController.java
@@ -3,11 +3,15 @@ package com.spring.jobrunr.api;
 import com.spring.jobrunr.jobs.SampleJobRequest;
 import com.spring.jobrunr.services.SampleJobService;
 import org.jobrunr.jobs.JobId;
+import org.jobrunr.scheduling.BackgroundJob;
 import org.jobrunr.scheduling.JobRequestScheduler;
 import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
 
 @RestController
 public class JobController {
@@ -32,6 +36,12 @@ public class JobController {
     public String enqueueExampleViaJobRequest(@RequestParam(value = "name", defaultValue = "World") String name) {
         final JobId enqueuedJobId = jobRequestScheduler.enqueue(new SampleJobRequest(name));
         return "Job Enqueued: " + enqueuedJobId.toString();
+    }
+
+    @GetMapping("/via-job-request/delayed")
+    public String scheduleDelayedJob(@RequestParam(name = "duration", defaultValue = "5") Integer duration) {
+        final JobId jobId = jobRequestScheduler.schedule(LocalDateTime.now().plusMinutes(duration), new SampleJobRequest("Delayed By => "+ duration));
+        return "Job delayed By " + jobId.toString();
     }
 
 }
--- src/main/java/com/spring/jobrunr/jobs/SampleJob.java
+++ src/main/java/com/spring/jobrunr/jobs/SampleJob.java
@@ -1,6 +1,7 @@
 package com.spring.jobrunr.jobs;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jobrunr.jobs.annotations.Job;
 import org.jobrunr.scheduling.BackgroundJob;
 import org.jobrunr.scheduling.JobScheduler;
@@ -11,16 +12,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SampleJob {
 
 
     private final JobScheduler jobScheduler;
 
 
-    @Recurring(id = "my-recurring-job", cron = "*/2 * * * *")
+    @Recurring(id = "my-recurring-job", cron = "*/1 * * * *")
     @Job(name = "A recurring job")
     public void recurringJob() throws InterruptedException {
         System.out.println("The recurring job has begun.");
@@ -31,6 +35,18 @@ public class SampleJob {
             throw e;
         } finally {
             System.out.println("Recurring job has finished...");
+        }
+    }
+
+    @Recurring(id = "Test-retry-Recurring-Job", cron = "*/10 * * * *")
+    @Job(name = "Retry Recurring Job", retries = 3)
+    public void recurringRetryJob() {
+        boolean flag = true;
+        Integer value = ThreadLocalRandom.current().nextInt(0, 100);
+        if (value > 20) {
+            throw new RuntimeException("A Random Exception ....");
+        } else {
+            log.info("Successfully Retried ..... ");
         }
     }
 }
